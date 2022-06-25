affed23f973bce8840ede115c06450450f3a19b1
Etienne Thompson <43585499+EtienneThompson@users.noreply.github.com>
2022-06-25 16:12:36 -0700

    Add notion automation script. (#28)
    * Create notion items based on created time.
    
    * If date set on previous ticket, set new item date to frequency later.
    
    * Use period date instead of created time for filtering.
    
    * Added logging.

--- cloud/functions/notion/createWorkItems.js
+++ cloud/functions/notion/createWorkItems.js
@@ -5,11 +5,86 @@ dotenv.config();
 
 const notion = new Client({ auth: process.env.NOTION_KEY });
 
+function getThisWeekMonday() {
+  let monday = new Date();
+  let adder = [-6, 0, -1, -2, -3, -4, -5];
+  // This gives a number in the range (0 - 6) where 1 in Sunday.
+  let currentDay = monday.getDay();
+  // Use the current day to find out how much to offset the current date by to
+  // get this week's monday.
+  monday.setDate(monday.getDate() + adder[currentDay]);
+  return monday.toISOString().split("T")[0];
+}
+
+function generatePreviousFrequencyDate(frequency, date) {
+  let previous_date;
+  if (date) {
+    previous_date = new Date(date);
+  } else {
+    previous_date = new Date();
+  }
+  switch (frequency) {
+    case "Weekly":
+      previous_date.setDate(previous_date.getDate() - 7);
+      break;
+
+    case "Bi-Weekly":
+      previous_date.setDate(previous_date.getDate() - 14);
+      break;
+
+    case "Monthly":
+      previous_date.setMonth(previous_date.getMonth() - 1);
+      break;
+
+    case "Annually":
+      previous_date.setFullYear(previous_date.getFullYear() - 1);
+      break;
+
+    default:
+      console.log("default");
+      break;
+  }
+
+  return previous_date.toISOString().split("T")[0];
+}
+
+function generateFutureFrequencyDate(frequency, date) {
+  let previous_date;
+  if (date) {
+    previous_date = new Date(date);
+  } else {
+    previous_date = new Date();
+  }
+  switch (frequency) {
+    case "Weekly":
+      previous_date.setDate(previous_date.getDate() + 7);
+      break;
+
+    case "Bi-Weekly":
+      previous_date.setDate(previous_date.getDate() + 14);
+      break;
+
+    case "Monthly":
+      previous_date.setMonth(previous_date.getMonth() + 1);
+      break;
+
+    case "Annually":
+      previous_date.setFullYear(previous_date.getFullYear() + 1);
+      break;
+
+    default:
+      console.log("default");
+      break;
+  }
+
+  return previous_date.toISOString().split("T")[0];
+}
+
 // Get a list of pages of clients.
-async function getAllDatabaseEntries() {
+async function getAllDatabaseEntries(database_id) {
   try {
     const response = await notion.databases.query({
-      database_id: process.env.CLIENT_DATABASE_ID,
+      database_id: database_id,
     });
 
     return response.results;
@@ -18,10 +93,44 @@ async function getAllDatabaseEntries() {
   }
 }
 
-// Create a work item with a title and two relation properties set.
-async function createWorkItem(title, clientId, serviceId, database) {
+async function getExistingWorkItem(database_id, clientId, serviceId, query_date) {
   try {
-    const response = await notion.pages.create({
+    const response = await notion.databases.query({
+      database_id: database_id,
+      filter: {
+        and: [
+          {
+            property: "Period Date",
+            date: {
+              on_or_after: query_date,
+            },
+          },
+          {
+            property: "Client",
+            relation: {
+              contains: clientId,
+            },
+          },
+          {
+            property: "Service",
+            relation: {
+              contains: serviceId,
+            },
+          },
+        ],
+      },
+    });
+    return response.results;
+  } catch (error) {
+    console.log(error.body);
+    return [];
+  }
+}
+
+// Create a work item with a title and two relation properties set.
+async function createWorkItem(title, monday, clientId, serviceId, startTime, database) {
+  try {
+    let requestBody = {
       parent: { database_id: database },
       properties: {
         title: {
@@ -43,29 +152,111 @@ async function createWorkItem(title, clientId, serviceId, database) {
         Service: {
           relation: [serviceId],
         },
+        "Period Date": {
+          date: {
+            start: monday,
+          },
+        },
       },
-    });
+    };
+    // If a time to set was specified, add it to the request.
+    if (startTime) {
+      requestBody.properties.Date = {
+        date: {
+          start: startTime,
+        },
+      };
+    }
+    const response = await notion.pages.create(requestBody);
   } catch (error) {
     console.log(error.body);
   }
 }
 
-let pages = await getAllDatabaseEntries();
+async function main() {
+  console.log("Getting list of clients...");
+  let clients = await getAllDatabaseEntries(process.env.CLIENT_DATABASE_ID);
+  console.log("List of clients acquired.");
 
-// Create the needed work items.
-for (let page of pages) {
-  let clientId = page.id;
-  let clientName;
-  let clientServices = [];
-  for (let key of Object.keys(page.properties)) {
-    if (key === "Name") {
-      clientName = page.properties[key].title[0].plain_text;
-    } else if (key === "Services") {
-      clientServices = page.properties[key].relation;
+  console.log("Getting list of services...");
+  let services = await getAllDatabaseEntries(process.env.SERVICE_DATABASE_ID);
+  console.log("List of services acquired.");
+
+  let serviceMap = {};
+
+  console.log("Generating service map...");
+  // Generate a map between service id and information
+  for (let service of services) {
+    serviceMap[service.id] = {
+      service_name: service.properties["Name"].title[0].plain_text,
+      freq_id: service.properties["Frequency"].select.id,
+      freq_name: service.properties["Frequency"].select.name,
+    };
+  }
+
+  console.log("Creating work items...");
+  // Create the needed work items.
+  for (let client of clients) {
+    let clientId = client.id;
+    let clientName;
+    let clientServices = [];
+    // Get the useful information from the client.
+    for (let key of Object.keys(client.properties)) {
+      if (key === "Name") {
+        clientName = client.properties[key].title[0].plain_text;
+      } else if (key === "Services") {
+        clientServices = client.properties[key].relation;
+      }
+    }
+
+    console.log();
+    console.log(`Checking client ${clientName}...`);
+
+    for (let serviceId of clientServices) {
+      // Calculate date for frequency.
+      let freq = serviceMap[serviceId.id].freq_name;
+
+      // Custom frequency services are created manually.
+      if (freq === "Custom") {
+        continue;
+      }
+
+      // Instead, get the date for the current week's monday, and check if an
+      // item exists within the past frequency of that monday.
+      let monday = getThisWeekMonday();
+      let previous_date = generatePreviousFrequencyDate(freq, monday);
+
+      // Check if a ticket that was created within the past frequency amount of
+      // time exists. If it does, skip. Otherwise, create the work item.
+      // Ignore any custom services.
+      let existingItems = await getExistingWorkItem(
+        process.env.WORK_ITEM_DATABASE_ID,
+        clientId,
+        serviceId.id,
+        previous_date
+      );
+      if (existingItems.length > 0) {
+        continue;
+      }
+
+      console.log(`Need to create new work item for service ${serviceMap[serviceId.id].service_name}...`);
+
+      // Get the date that was assigned to the previous tickets.
+      let filter_date = generatePreviousFrequencyDate(freq, previous_date);
+      let lastItem = await getExistingWorkItem(process.env.WORK_ITEM_DATABASE_ID, clientId, serviceId.id, filter_date);
+      let newDate = "";
+      // If there was a date assigned, then generate when the next one should be.
+      if (lastItem.length > 0 && lastItem[0].properties["Date"].date) {
+        let lastDate = lastItem[0].properties["Date"].date.start;
+        newDate = generateFutureFrequencyDate(freq, lastDate);
+      }
+
+      console.log("Creating work item...");
+      // Create the new work item.
+      await createWorkItem(clientName, monday, clientId, serviceId, newDate, process.env.WORK_ITEM_DATABASE_ID);
+      console.log("Work item created successfully.");
     }
   }
-
-  for (let serviceId of clientServices) {
-    await createWorkItem(clientName, clientId, serviceId, process.env.WORK_ITEM_DATABASE_ID);
-  }
 }
+
+await main();
