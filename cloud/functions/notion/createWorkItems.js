8fe487538fc9e8ec39ca4eaad0fdc0f0b8b9330c
Etienne Thompson <et@etiennethompson.com>
2022-06-25 16:11:24 -0700

    Added logging.

--- cloud/functions/notion/createWorkItems.js
+++ cloud/functions/notion/createWorkItems.js
@@ -173,73 +173,90 @@ async function createWorkItem(title, monday, clientId, serviceId, startTime, dat
   }
 }
 
-let clients = await getAllDatabaseEntries(process.env.CLIENT_DATABASE_ID);
+async function main() {
+  console.log("Getting list of clients...");
+  let clients = await getAllDatabaseEntries(process.env.CLIENT_DATABASE_ID);
+  console.log("List of clients acquired.");
 
-let services = await getAllDatabaseEntries(process.env.SERVICE_DATABASE_ID);
+  console.log("Getting list of services...");
+  let services = await getAllDatabaseEntries(process.env.SERVICE_DATABASE_ID);
+  console.log("List of services acquired.");
 
-let serviceMap = {};
+  let serviceMap = {};
 
-for (let service of services) {
-  serviceMap[service.id] = {
-    service_name: service.properties["Name"].title[0].plain_text,
-    freq_id: service.properties["Frequency"].select.id,
-    freq_name: service.properties["Frequency"].select.name,
-  };
-}
-
-// Create the needed work items.
-for (let client of clients) {
-  let clientId = client.id;
-  let clientName;
-  let clientServices = [];
-  for (let key of Object.keys(client.properties)) {
-    if (key === "Name") {
-      clientName = client.properties[key].title[0].plain_text;
-    } else if (key === "Services") {
-      clientServices = client.properties[key].relation;
-    }
+  console.log("Generating service map...");
+  // Generate a map between service id and information
+  for (let service of services) {
+    serviceMap[service.id] = {
+      service_name: service.properties["Name"].title[0].plain_text,
+      freq_id: service.properties["Frequency"].select.id,
+      freq_name: service.properties["Frequency"].select.name,
+    };
   }
 
-  for (let serviceId of clientServices) {
-    // Calculate date for frequency.
-    let freq = serviceMap[serviceId.id].freq_name;
-
-    if (freq === "Custom") {
-      console.log("Skipping custom service");
-      continue;
+  console.log("Creating work items...");
+  // Create the needed work items.
+  for (let client of clients) {
+    let clientId = client.id;
+    let clientName;
+    let clientServices = [];
+    // Get the useful information from the client.
+    for (let key of Object.keys(client.properties)) {
+      if (key === "Name") {
+        clientName = client.properties[key].title[0].plain_text;
+      } else if (key === "Services") {
+        clientServices = client.properties[key].relation;
+      }
     }
 
-    // Instead, get the date for the current week's monday, and check if an
-    // item exists within the past frequency of that monday.
-    let monday = getThisWeekMonday();
-    let previous_date = generatePreviousFrequencyDate(freq, monday);
+    console.log();
+    console.log(`Checking client ${clientName}...`);
 
-    // Check if a ticket that was created within the past frequency amount of
-    // time exists. If it does, skip. Otherwise, create the work item.
-    // Ignore any custom services.
-    let existingItems = await getExistingWorkItem(
-      process.env.WORK_ITEM_DATABASE_ID,
-      clientId,
-      serviceId.id,
-      previous_date
-    );
-    if (existingItems.length > 0) {
-      continue;
+    for (let serviceId of clientServices) {
+      // Calculate date for frequency.
+      let freq = serviceMap[serviceId.id].freq_name;
+
+      // Custom frequency services are created manually.
+      if (freq === "Custom") {
+        continue;
+      }
+
+      // Instead, get the date for the current week's monday, and check if an
+      // item exists within the past frequency of that monday.
+      let monday = getThisWeekMonday();
+      let previous_date = generatePreviousFrequencyDate(freq, monday);
+
+      // Check if a ticket that was created within the past frequency amount of
+      // time exists. If it does, skip. Otherwise, create the work item.
+      // Ignore any custom services.
+      let existingItems = await getExistingWorkItem(
+        process.env.WORK_ITEM_DATABASE_ID,
+        clientId,
+        serviceId.id,
+        previous_date
+      );
+      if (existingItems.length > 0) {
+        continue;
+      }
+
+      console.log(`Need to create new work item for service ${serviceMap[serviceId.id].service_name}...`);
+
+      // Get the date that was assigned to the previous tickets.
+      let filter_date = generatePreviousFrequencyDate(freq, previous_date);
+      let lastItem = await getExistingWorkItem(process.env.WORK_ITEM_DATABASE_ID, clientId, serviceId.id, filter_date);
+      let newDate = "";
+      // If there was a date assigned, then generate when the next one should be.
+      if (lastItem.length > 0 && lastItem[0].properties["Date"].date) {
+        let lastDate = lastItem[0].properties["Date"].date.start;
+        newDate = generateFutureFrequencyDate(freq, lastDate);
+      }
+
+      console.log("Creating work item...");
+      // Create the new work item.
+      await createWorkItem(clientName, monday, clientId, serviceId, newDate, process.env.WORK_ITEM_DATABASE_ID);
+      console.log("Work item created successfully.");
     }
-
-    // Get the date that was assigned to the previous tickets.
-    let filter_date = generatePreviousFrequencyDate(freq, previous_date);
-    let lastItem = await getExistingWorkItem(process.env.WORK_ITEM_DATABASE_ID, clientId, serviceId.id, filter_date);
-    let newDate = "";
-    // If there was a date assigned, then generate when the next one should be.
-    if (lastItem.length > 0 && lastItem[0].properties["Date"].date) {
-      let lastDate = lastItem[0].properties["Date"].date.start;
-      console.log(lastDate);
-      newDate = generateFutureFrequencyDate(freq, lastDate);
-      console.log(newDate);
-    }
-
-    // Create the new work item.
-    await createWorkItem(clientName, monday, clientId, serviceId, newDate, process.env.WORK_ITEM_DATABASE_ID);
   }
 }
+
+await main();
