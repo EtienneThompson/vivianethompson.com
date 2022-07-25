4ddbf2d6aad9d5c94b79de7943007beb65ee1443
Etienne Thompson <et@etiennethompson.com>
2022-07-24 19:01:03 -0700

    Add individual team member component.

---
+++ website/src/components/about/strings.js
@@ -0,0 +1,22 @@
+let viviane_blurb =
+  "You will notice her accent right away, yes, it’s sounds German but she is French. Viviane came to the US after high school and worked while going to college. Upon taking an accounting class she discovered her passion, and has fueled her carreer since then. Viviane got her CPA certificate in 1994 and has been working for and with small business owners since. After moving to Tucson in 2005, she decided to open an accounting and tax practice rather than work for a single employer. She is married to Dave Thompson, an astronomer working for the Large Binocular Telescope (part of the University of Arizona) and has a 20 year old son (he made this website!) who is enjoying his studies in Computer Science and Chinese.";
+
+let paula_blurb =
+  "Paula graduated from the University of Arizona in December 2013 with a bachelor’s degree in Accounting. Her plans in 2020 are to continue her education and pass the CPA exam within the next two years. She has been in the bookkeeping and accounting field since 2006 and has worked for Thompson Accounting for the past 3 years wearing many different hats: accountant, tax preparer, and office manager. She is originally from California, and has called Tucson home since 2003. Her black, furry, needy and gift-giving cat is named JJ.";
+
+let michala_blurb =
+  "Michala and her husband Jon, recently moved to Tucson from Portland, OR after visiting the area on vacation and loving the heat and dryness. She joined our practice as an accountant bringing a lot of experience working with small business owners in addition to being a mom (Joshua is 2 years old) and loving dogs. She is excited to further her career in business along with getting her college degree.";
+
+let monique_blurb =
+  "Monique has lived in Tucson for 33 years, is young at heart, and loves numbers. She is pursuing a bachelor's degree in accounting.";
+
+let leslie_blurb =
+  "Leslie moved to Tucson with her husband in 2017 from Oregon, bringing her vast experience and numerous skills acquired over 20 years working in various administrative and clerical positions. She is comfortable with technology and proficient in many software programs including QuickBooks. She is also an accomplished Chef who has owned and operated her own restaurant, from cooking to managing all aspects of the restaurant business including creating and maintaining the company website, customer relations, employee supervision, as well as co-authoring and publishing a cookbook with her Italian Chef Husband.";
+
+module.exports = {
+  viviane_blurb,
+  paula_blurb,
+  michala_blurb,
+  monique_blurb,
+  leslie_blurb
+};
