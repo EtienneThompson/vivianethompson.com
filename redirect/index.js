7207f9347a6fb706f27c51cc33e41c615db14867
Etienne Thompson <et@etiennethompson.com>
2022-07-29 09:49:55 -0700

    Update redirect site to have a delay.

--- redirect/index.js
+++ redirect/index.js
@@ -1,1 +1,16 @@
-window.location.href = "https://vivianethompson.com";
+let seconds = 5;
+
+document.getElementById("countdown-text").innerText = `You will be automatically redirected in ${seconds} seconds.`;
+
+setInterval(function () {
+  if (seconds === 0) {
+    return;
+  }
+
+  seconds = seconds - 1;
+  document.getElementById("countdown-text").innerText = `You will be automatically redirected in ${seconds} seconds.`;
+}, 1000);
+
+setTimeout(function () {
+  window.location.href = "https://vivianethompson.com";
+}, 5000);
